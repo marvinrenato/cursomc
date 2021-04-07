9426a08a375b2919ccb952918b7a9ecd08371af5
Marvin <marvin.renato@gmail.com>
2021-04-06 21:10:11 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/marvin/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.marvin.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marvin.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
